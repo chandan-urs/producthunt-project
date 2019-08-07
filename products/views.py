c881574ee06a01d17254c7c4b695178a6a5b3fb8
Chandan <p.chandan1995@gmail.com>
2019-08-07 21:52:48 +0530

    product hunt done

--- products/views.py
+++ products/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {"products": products})
 
 
 @login_required
@@ -26,8 +27,20 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('products/'+product.id)
         else:
             return render(request,'products/create.html', {"error": "All fields are required!"})
     else:
         return render(request,'products/create.html')
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {"product": product})
+
+@login_required
+def upvote(request,product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.votes_total = product.votes_total + 1
+    product.save()
+    return redirect('/products/' + str(product_id))
